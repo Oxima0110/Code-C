0a5baf19a42021768ca0bfb48aec20e6325a45b9
Oxana <crazyglass@list.ru>
2022-06-26 11:17:25 +0300

    методы заполнения, печати массива, нахождения заданного числа

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿// метод заполнение массива
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+// метод печать массива
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+// метод нахождения заданного числа
+int InexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+// задаем длину массива в 10
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = InexOf(array, 4);
+Console.WriteLine(pos);
