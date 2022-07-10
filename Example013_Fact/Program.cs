86019ab9e52b1801c6c95f9a14d6c11a4efe647e
Oxana <crazyglass@list.ru>
2022-07-10 20:29:49 +0300

    Факториал и Фибоначчи

---
+++ Example013_Fact/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вычисление факториала рекурсия
+
+Console.Clear();
+
+int Factorial(int n)
+{
+    //1! = 1
+    //0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(3));
+
+// числа фибоначчи
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
+
